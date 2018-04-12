31ff92dd1042c931cf625584994634ac9b3a1de4
m305-29\w401 <egg880604@gmail.com>
2018-04-12 17:36:39 +0800

    作業完成

---
+++ rand2/rand2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rand2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.將所有號碼都放入List之中
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= 42; i++)
+            {
+                numbers.Add(i);
+            }
+
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                //2.隨機挑選其中一個印出
+                int r = rand.Next(1, numbers.Count);
+                Console.WriteLine(numbers[r] + "");
+
+                //3.並從List中刪除
+                numbers.RemoveAt(r);
+
+                //4.重複2.3六次
+            }
+                Console.ReadKey();
+
+            
+        }
+    }
+}
+
